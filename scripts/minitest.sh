d968f8d9729470bb8e0596690471359dfbcfe3f8
ころん:すとりーむ <37651140+big-stream@users.noreply.github.com>
2018-09-21 15:03:42 +0900

    更新: README.md, help.js, package.json, minitest.sh: v0.3.1

--- scripts/minitest.sh
+++ scripts/minitest.sh
@@ -4,7 +4,7 @@ cd ${script%/*/*} # リポジトリ
 
 echo "miniテスト開始:"
 
-md5='45db24adbe3c92ce52daf90a306e70cf  -'
+md5='7c98bf9bb47d298483ce8a69243ccdc1  -'
 css='code{background-color:#cccccc;}'
 ruby=(--ruby '不味い:ん?! マジぃ')
 check=$( rubygana --md-html README.md | rubygana --html --css "$css" "${ruby[@]}" --comment | tee README.md.rubygana.html | md5sum )
