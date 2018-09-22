d6b8d63d2dabac3e3ee3259c3f3689cd0eb9a589
ころん:すとりーむ <37651140+big-stream@users.noreply.github.com>
2018-09-22 21:23:54 +0900

    更新: README.md, help.js, package.json, minitest.sh: v0.5.0

--- README.md
+++ README.md
@@ -16,7 +16,7 @@
 ルビ族以外もどうぞ!
 
 
-## 特徴(rubygana 0.4.0)
+## 特徴(rubygana 0.5.0)
 
 1. ##### HTMLのルビ化
    `<ruby><rb>蝙蝠</rb><rt>こうもり</rt></ruby>`
--- scripts/minitest.sh
+++ scripts/minitest.sh
@@ -4,9 +4,19 @@ cd ${script%/*/*} # リポジトリ
 
 echo "miniテスト開始:"
 
-md5='fa5a5506e97c090a25a1cdbeb916c77e  -'
+md5='eaa6e06649cc0a1f2537e25ea1a96810  -'
 css='code{background-color:#cccccc;}'
-ruby=(--ruby '不味い:ん?! マジぃ')
+ruby=(
+--ruby '不味い:ん?! マジぃ'
+--ruby-re '[0-9一二三四五]行:行:ぎょう'
+--ruby '空行:くうぎょう'
+--ruby '冒頭行:行:ぎょう'
+--ruby-re '行(ごと|毎):行:ぎょう'
+--ruby-re '行[をはが]:行:ぎょう'
+--ruby '粒度:りゅうど'
+--ruby '文科省:もんかしょう'
+--ruby '空:から'
+)
 check=$( rubygana --md-html README.md | rubygana --html --css "$css" "${ruby[@]}" --comment | tee README.md.rubygana.html | md5sum )
 [[ $md5 = $check ]] || { echo "$check"; exit 1; }
 
