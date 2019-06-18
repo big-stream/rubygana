9181855245c8f1ed02142b994477e9e37b67dcae
ころん:すとりーむ <37651140+big-stream@users.noreply.github.com>
2019-06-18 16:56:59 +0900

    訂正: minitest.sh: md5

--- scripts/minitest.sh
+++ scripts/minitest.sh
@@ -4,7 +4,7 @@ cd ${script%/*/*} # リポジトリ
 
 echo "miniテスト開始:"
 
-md5='b8e9abdbda84b25ebcd08a3a152532ec  -'
+md5='94713afc3e1ea459c830bed13e62171c  -'
 check=$( rubygana --readme-html | tee README.md.rubygana.html | md5sum )
 [[ $md5 = $check ]] || { echo "$check"; exit 1; }
 
